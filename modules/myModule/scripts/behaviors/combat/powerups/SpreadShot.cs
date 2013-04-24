60c7e1f5e48da35c8af47a64b61ac8a695c86be3
Bill Tyros <tyros.bill@gmail.com>
2013-04-23 21:57:35 -0400

    add looping to createSpreadShot

--- modules/myModule/scripts/behaviors/combat/powerups/SpreadShot.cs
+++ modules/myModule/scripts/behaviors/combat/powerups/SpreadShot.cs
@@ -60,126 +60,35 @@ function SpreadShotBehavior::spreadShot(%this)
 function SpreadShotBehavior::createSpreadShot(%this)
 {
    %adjustedAngle = getPositiveAngle(%this.owner);
-   
    //Calculate a direction from an Angle and Magnitude
    %shotOffset= Vector2Direction(%adjustedAngle,%this.owner.Size.height * 0.7);
    
-   // Create the sprite.
-   %shot1 = new Sprite()
+   %angles = "0 -8 8 -16 16";
+   for(%i = 0; %i < getWordCount(%angles); %i++)
    {
-      class = SpreadShot;
-      Animation = "ToyAssets:Cannonball_Projectile_3Animation";
-      BodyType = dynamic;
-      Bullet = true;
-      Position = (%this.owner.Position.x + %shotOffset.x) SPC (%this.owner.Position.y + %shotOffset.y);
-      Size = 2;
-      SceneLayer = 1;
-      SceneGroup = 2;
-      CollisionCallback = true;
-   };
-   
-   %shot2 = new Sprite()
-   {
-      class = SpreadShot;
-      Animation = "ToyAssets:Cannonball_Projectile_3Animation";
-      BodyType = dynamic;
-      Bullet = true;
-      Position = (%this.owner.Position.x + %shotOffset.x) SPC (%this.owner.Position.y + %shotOffset.y);
-      Size = 2;
-      SceneLayer = 1;
-      SceneGroup = 2;
-      CollisionCallback = true;
-   };
-   
-   %shot3 = new Sprite()
-   {
-      class = SpreadShot;
-      Animation = "ToyAssets:Cannonball_Projectile_3Animation";
-      BodyType = dynamic;
-      Bullet = true;
-      Position = (%this.owner.Position.x + %shotOffset.x) SPC (%this.owner.Position.y + %shotOffset.y);
-      Size = 2;
-      SceneLayer = 1;
-      SceneGroup = 2;
-      CollisionCallback = true;
-   };
-   
-   %shot4 = new Sprite()
-   {
-      class = SpreadShot;
-      Animation = "ToyAssets:Cannonball_Projectile_3Animation";
-      BodyType = dynamic;
-      Bullet = true;
-      Position = (%this.owner.Position.x + %shotOffset.x) SPC (%this.owner.Position.y + %shotOffset.y);
-      Size = 2;
-      SceneLayer = 1;
-      SceneGroup = 2;
-      CollisionCallback = true;
-   };
-   
-   %shot5 = new Sprite()
-   {
-      class = SpreadShot;
-      Animation = "ToyAssets:Cannonball_Projectile_3Animation";
-      BodyType = dynamic;
-      Bullet = true;
-      Position = (%this.owner.Position.x + %shotOffset.x) SPC (%this.owner.Position.y + %shotOffset.y);
-      Size = 2;
-      SceneLayer = 1;
-      SceneGroup = 2;
-      CollisionCallback = true;
-   };
-   %shot1.setLinearVelocityPolar(%this.owner.Angle - 180,%this.speed);
-   %shot2.setLinearVelocityPolar(%this.owner.Angle - 180 + 8,%this.speed);
-   %shot3.setLinearVelocityPolar(%this.owner.Angle - 180 - 8,%this.speed);
-   %shot4.setLinearVelocityPolar(%this.owner.Angle - 180 + 16,%this.speed);
-   %shot5.setLinearVelocityPolar(%this.owner.Angle - 180 - 16,%this.speed);
-
-   //Sets the collision shape to a circle
-   %shot1.createCircleCollisionShape( 0.7, "-0.1 0.6" );
-   %shot1.setCollisionGroups("0 1 4");
-   %shot2.createCircleCollisionShape( 0.7, "-0.1 0.6" );
-   %shot2.setCollisionGroups("0 1 4");
-   %shot3.createCircleCollisionShape( 0.7, "-0.1 0.6" );
-   %shot3.setCollisionGroups("0 1 4");
-   %shot4.createCircleCollisionShape( 0.7, "-0.1 0.6" );
-   %shot4.setCollisionGroups("0 1 4");
-   %shot5.createCircleCollisionShape( 0.7, "-0.1 0.6" );
-   %shot5.setCollisionGroups("0 1 4");
-   
-   %dealDmgBehavior1 = DealsDamageBehavior.createInstance();
-   %dealDmgBehavior1.strength = %this.damage;
-   %shot1.addBehavior(%dealDmgBehavior1);
-   
-   %dealDmgBehavior2 = DealsDamageBehavior.createInstance();
-   %dealDmgBehavior2.strength = %this.damage;
-   %shot2.addBehavior(%dealDmgBehavior2);
-   
-   %dealDmgBehavior3 = DealsDamageBehavior.createInstance();
-   %dealDmgBehavior3.strength = %this.damage;
-   %shot3.addBehavior(%dealDmgBehavior3);
-   
-   %dealDmgBehavior4 = DealsDamageBehavior.createInstance();
-   %dealDmgBehavior4.strength = %this.damage;
-   %shot4.addBehavior(%dealDmgBehavior4);
-   
-   %dealDmgBehavior5 = DealsDamageBehavior.createInstance();
-   %dealDmgBehavior5.strength = %this.damage;
-   %shot5.addBehavior(%dealDmgBehavior5);
-   
-   %shot1.dieSchedule = %shot1.schedule(%this.spreadShotLifespan,destroy);
-   %shot2.dieSchedule = %shot2.schedule(%this.spreadShotLifespan,destroy);
-   %shot3.dieSchedule = %shot3.schedule(%this.spreadShotLifespan,destroy);
-   %shot4.dieSchedule = %shot4.schedule(%this.spreadShotLifespan,destroy);
-   %shot5.dieSchedule = %shot5.schedule(%this.spreadShotLifespan,destroy);
-   
-   // Add the sprite to the scene.
-   %ownerScene = %this.owner.getScene();   
-   %ownerScene.add( %shot1 );
-   %ownerScene.add( %shot2 );
-   %ownerScene.add( %shot3 );
-   %ownerScene.add( %shot4 );
-   %ownerScene.add( %shot5 );
+      %shot = new Sprite()
+      {
+         class = SpreadShot;
+         Animation = "ToyAssets:Cannonball_Projectile_3Animation";
+         BodyType = dynamic;
+         Bullet = true;
+         Position = (%this.owner.Position.x + %shotOffset.x) SPC (%this.owner.Position.y + %shotOffset.y);
+         Size = 2;
+         SceneLayer = 1;
+         SceneGroup = 2;
+         CollisionCallback = true;
+      };
+      %shot.setLinearVelocityPolar(%this.owner.Angle - 180 + getWord(%angles, %i),%this.speed);
+      %shot.createCircleCollisionShape( 0.7, "-0.1 0.6" );
+      %shot.setCollisionGroups("0 1 4");
+      
+      %dealDmgBehavior = DealsDamageBehavior.createInstance();
+      %dealDmgBehavior.strength = %this.damage;
+      %shot.addBehavior(%dealDmgBehavior);
+      
+      %shot.dieSchedule = %shot.schedule(%this.spreadShotLifespan,destroy);
+      %this.owner.getScene().add( %shot );
+   }
 }
 
 function SpreadShot::onCollision(%this, %object, %details)
