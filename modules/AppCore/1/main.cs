6f582ca3dcd6e492f361ebb19eb2e151d3c4b4a9
Bill Tyros <tyros.bill@gmail.com>
2013-04-24 11:56:21 -0400

    increment version number

--- modules/AppCore/1/main.cs
+++ modules/AppCore/1/main.cs
@@ -29,7 +29,7 @@ function AppCore::create( %this )
     exec("./scripts/openal.cs");
     
     // Initialize the canvas
-    initializeCanvas("Mega Epic Super Tankz v0.01 ALPHA");
+    initializeCanvas("Mega Epic Super Tankz v0.02 ALPHA");
     
     // Set the canvas color
     Canvas.BackgroundColor = "CornflowerBlue";
