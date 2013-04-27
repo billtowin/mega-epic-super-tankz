6397fdc53396be4a15ea2defdbd8bab28ca1d67f
Bill Tyros <tyros.bill@gmail.com>
2013-04-27 17:43:00 -0400

    improve laserBeam collision behavior

--- modules/myModule/scripts/behaviors/combat/powerups/LaserBeam.cs
+++ modules/myModule/scripts/behaviors/combat/powerups/LaserBeam.cs
@@ -7,14 +7,14 @@ if (!isObject(LaserBeamBehavior))
    %template.description  = "Tank Laser Shot projectile";
 
    %template.addBehaviorField(duration, "Duration of Powerup/Ability (negative number for infinite) (in ms)", int, 15000);
-   %template.addBehaviorField(damage, "Damage per tick/refresh", int, 5);
+   %template.addBehaviorField(damage, "Damage per tick/refresh", int, 3);
 
    %template.addBehaviorField(beamWidth, "Width of Laser beam", float, 0.8);
    %template.addBehaviorField(beamLength, "Range/Length of laser beam", float, 22);
    
    %template.addBehaviorField(reloadTime, "Reload time (in ms)", int, 2500);
    %template.addBehaviorField(maxBeamTime, "Max Beam Time", int, 600);
-   %template.addBehaviorField(beamRefreshTime, "Time between beam ticks/refreshes for damage and positioning", int, 50);
+   %template.addBehaviorField(beamRefreshTime, "Time between beam ticks/refreshes for damage and positioning", int, 40);
 }
 
 function LaserBeamBehavior::onBehaviorAdd(%this)
@@ -59,6 +59,7 @@ function LaserBeamBehavior::updateBeamPosition(%this)
    if(%this.timeSpentBeaming <= %this.maxBeamTime)
    {
       %this.currentBeam.Position = %this.owner.getWorldPoint( 0 SPC (%this.owner.Size.height / 2 + %this.beamLength / 1.9));
+      %this.currentBeam.Angle = %this.owner.Angle;
       %this.beamSchedule = %this.schedule(%this.beamRefreshTime, updateBeamPosition);
    } else {
       cancel(%this.beamingSchedule);
@@ -89,34 +90,38 @@ function LaserBeamBehavior::createBeam(%this)
       Size = %this.beamWidth SPC %this.beamLength;
       SceneLayer = 2;
       SceneGroup = 2;
-      Position = %this.owner.getWorldPoint( 0 SPC (%this.owner.Size.height / 2 + %this.beamLength / 1.9));
+      Position = %this.owner.getWorldPoint( 0 SPC (%this.owner.Size.height / 2 + %this.beamLength / 1.6));
    };
    
+   %beam.damage = %this.damage;
+   %beam.beamRefreshTime = %this.beamRefreshTime;
    %beam.dieSchedule = %beam.schedule(%this.maxBeamTime,safeDelete);
-   %beam.collisionCheckSchedule = %this.schedule(%this.beamRefreshTime, checkForCollisionsManually);
+   %beam.collisionCheckSchedule = %beam.schedule(%this.beamRefreshTime, checkForCollisionsManually);
    
-   %this.currentBeam = %beam;
    %this.owner.getScene().add( %beam );
+   %this.currentBeam = %beam;
 }
 
 function LaserBeam::checkForCollisionsManually(%this)
 {
-   %startP = %this.getWorldPoint(0 SPC (- %this.Size.height));
-   %endP = %this.getWorldPoint(0 SPC %this.Size.height);
-   %rayPicked = %this.owner.getScene().pickRayCollision(%startP, %endP, -1,-1);
-   for(%i = 0 ; %i < getWordCount(%rayPicked); %i++) 
+   %startP = %this.getWorldPoint(0 SPC (- %this.Size.height / 2));
+   %endP = %this.getWorldPoint(0 SPC %this.Size.height / 2);
+   %rayPicked = %this.getScene().pickRayCollision(%startP, %endP, -1,-1);
+   for(%i = 0 ; %i < getWordCount(%rayPicked); %i++)
    {
       %obj = getWord(%rayPicked, %i);
-      %takesDamage = %obj.getBehavior("TakesDamageBehavior");
-      if (isObject(%takesDamage)) {
-         %takesDamage.takeDamage(%this.damage);
-      }
-      
-      if(%obj.class $= "MineShot" || %obj.class $= "SpreadShot" || %obj.class $= "ChargeShot") {
-         %obj.onDeath();  
-      }
-      if(%obj.class $= "Scenery" && %obj.isBreakable) {
-         %obj.safeDelete();
+      if(isObject(%obj)) 
+      {
+         %takesDamage = %obj.getBehavior("TakesDamageBehavior");
+         if (isObject(%takesDamage)) {
+            %takesDamage.takeDamage(%this.damage);
+         }
+         if(%obj.class $= "MineShot" || %obj.class $= "SpreadShot" || %obj.class $= "ChargeShot") {
+            %obj.onDeath();  
+         }
+         if(%obj.class $= "Scenery" && %obj.isBreakable) {
+            %obj.safeDelete();
+         }
       }
    }
    %this.collisionCheckSchedule = %this.schedule(%this.beamRefreshTime, checkForCollisionsManually);
